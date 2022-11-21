414a871d13ff6661b2bdf21fe08445900d04e4d5
Thanh Tai <taint@zen-s.com>
2022-11-21 09:11:48 +0700

    impro

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.grey,
         ),
-        home: const GioiThieu());
+        home: const Welcome());
   }
 }
