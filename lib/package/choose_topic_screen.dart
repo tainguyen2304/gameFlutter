77e72e9a1659a13110e38e5aff3f2b5cc9e91e4a
Thanh Tai <taint@zen-s.com>
2022-12-09 09:31:06 +0700

    gett topic and levels from firebase

--- lib/package/choose_topic_screen.dart
+++ lib/package/choose_topic_screen.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:game/package/choose_level_play_offline.dart';
+import 'package:game/package/round_screen.dart';
 
 class ChooseTopic extends StatefulWidget {
   const ChooseTopic({Key? key}) : super(key: key);
@@ -20,52 +20,61 @@ class _ChooseTopicState extends State<ChooseTopic> {
         body: ListView(
       children: [
         Container(
-            padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+            padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
             width: MediaQuery.of(context).size.width,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                  image: AssetImage("images/backgroundGeneral.png"),
-                  fit: BoxFit.cover),
+                  image: AssetImage("images/h1.jpg"), fit: BoxFit.cover),
             ),
             child: StreamBuilder(
               stream: _topics.snapshots(),
               builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
                 if (streamSnapshot.hasData) {
-                  return Wrap(
-                      children: streamSnapshot.data!.docs
-                          .map((topicItem) => Container(
-                              margin: const EdgeInsets.all(4),
-                              child: ElevatedButton(
-                                style: ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStatePropertyAll<Color>(
-                                    const Color.fromARGB(255, 112, 235, 114)
-                                        .withOpacity(0.8),
-                                  ),
-                                  shape: MaterialStateProperty.all(
-                                    RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0)),
-                                  ),
-                                ),
-                                onPressed: () {
-                                  // Navigator.of(context).popUntil((route) => route.isFirst);
-                                  // Navigator.push(
-                                  //   context,
-                                  //   MaterialPageRoute(
-                                  //       builder: (context) => Round1()(
-                                  //           nickName: nickName.text, avatar: avatar, age: txtage.text)),
-                                  // );
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.all(2.0),
-                                  child: Text(
-                                    topicItem['name'],
-                                    style: const TextStyle(color: Colors.black),
-                                  ),
-                                ),
-                              )))
-                          .toList());
+                  return Column(
+                    children: [
+                      const Center(
+                        child: Text('Choose your topic',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold)),
+                      ),
+                      Wrap(
+                          children: streamSnapshot.data!.docs
+                              .map((topicItem) => Container(
+                                  margin: const EdgeInsets.all(4),
+                                  child: ElevatedButton(
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStatePropertyAll<Color>(
+                                        const Color.fromARGB(255, 112, 235, 114)
+                                            .withOpacity(0.8),
+                                      ),
+                                      shape: MaterialStateProperty.all(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10.0)),
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.of(context)
+                                          .popUntil((route) => route.isFirst);
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => Round(
+                                                  topic: topicItem['name'])));
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(2.0),
+                                      child: Text(
+                                        topicItem['name'],
+                                        style: const TextStyle(
+                                            color: Colors.black),
+                                      ),
+                                    ),
+                                  )))
+                              .toList())
+                    ],
+                  );
                 }
                 return const Center(
                   child: CircularProgressIndicator(),
